c915c07e2585affa00760f7f77388f01ab2ab195
lluo5779 <lluo5779@gmail.com>
2021-02-05 18:54:17 -0500

    Added multiprocessing

--- Modeling Bootcamp/run_experiment.py
+++ Modeling Bootcamp/run_experiment.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import json
 import os
 import datetime
+import time
 import numpy as np
+import multiprocessing as mp
 
 from mlflow import log_metric, log_param, log_artifacts, start_run, end_run, create_experiment
 
@@ -13,7 +15,9 @@ INSTANCE_FOLDER = "./data/Problems"
 SOLVER = minizinc.Solver.lookup("gecode")
 
 
-def run_minizinc_model(mdl_name, instance_name, timeout = 3600):
+def run_minizinc_model(params):
+    mdl_name, instance_name, timeout = params
+
     instance_path = os.path.join(INSTANCE_FOLDER, instance_name)
 
     if not os.path.isfile(mdl_name):
@@ -22,7 +26,7 @@ def run_minizinc_model(mdl_name, instance_name, timeout = 3600):
     if not os.path.isfile(instance_path):
         raise Exception("ERROR: instance_name not found: ", instance_name)
 
-    print("Processing model {} with instance {}".format(mdl_name, instance_name))
+    print("*** Processing model {} with instance {}".format(mdl_name, instance_name))
 
 
     model = minizinc.Model([mdl_name])
@@ -34,6 +38,9 @@ def run_minizinc_model(mdl_name, instance_name, timeout = 3600):
         input_data = json.load(f)
         scenesLength, actorsToScenes, actorsCosts = input_data['scenesLength'],input_data['actorsToScenes'], input_data['actorsCosts']
 
+
+    num_scenes = len(scenesLength)
+    num_actors = len(actorsCosts)
     if "1" in mdl_name:
 
         actorsToScenes_new = np.zeros((len(actorsCosts), len(scenesLength)))
@@ -68,47 +75,81 @@ def run_minizinc_model(mdl_name, instance_name, timeout = 3600):
 
     msol = instance.solve(verbose=True, timeout=datetime.timedelta(seconds=timeout))
 
-    return msol
+
+    # output= {
+    #
+    # }
+    if msol:
+        results = [
+            msol.status.name,
+            msol.solution.objective,
+            msol.statistics['initTime'].total_seconds(),
+            msol.statistics['solveTime'].total_seconds(),
+            msol.statistics['variables']]
+        if "1" in mdl_name:
+            scene_order = msol['scene_order']
+        else:
+            scene_order = msol['order']
+        success = True
 
 
+    else:
+        results = [msol.status.name if msol else "NOTFOUND"]
+        success = False
+        scene_order = None
 
-if __name__ == "__main__":
-    results = {"ids":[], "num_scenes":[], "num_actors":[], "obj":[], "init_time":[], "solve_time":[], "num_vars":[], "sln_status":[], "instance_name":[], "scene_order":[]}
-    no_sln = {"ids": [], "num_scenes":[], "num_actors":[], "sln_status": [], "instance_name":[]}
-    mdls = ['talent_model1.mzn', 'talent_model2.mzn']
-    timeout = 120
+    return success, mdl_name, instance_name, timeout, num_actors, num_scenes, scene_order, results
 
+def get_result(outputs):
+    global results, no_sln
+
+    for output in outputs:
+
+        success, mdl_name, instance_name, timeout, num_actors, num_scenes, scene_order, res = output
+
+        if success:
+            # results['ids'].append(mdl_i)
+            results['num_actors'].append(num_actors)
+            results['num_scenes'].append(num_scenes)
+            results["obj"].append(res[1])
+            results["init_time"].append(res[2])
+            results["solve_time"].append(res[3])
+            results['mdl_name'].append(mdl_name)
+            results['instance_name'].append(instance_name)
+            results['num_vars'].append(res[4])
+            results["sln_status"].append(res[0])
+
+            results['scene_order'].append(scene_order)
+
+        else:
+            # no_sln['ids'].append(mdl_i)
+            no_sln['num_scenes'].append(num_scenes)
+            no_sln['num_actors'].append(num_actors)
+            no_sln['mdl_name'].append(mdl_name)
+            no_sln['instance_name'].append(instance_name)
+            no_sln["sln_status"].append(res[0])
+    # print(results)
+    # print(no_sln)
+
+def run(mdls, timeout):
+    params = []
     for mdl_i, mdl_name in enumerate(mdls):
         for instance_name in os.listdir(INSTANCE_FOLDER):
             if '.json' in instance_name:
-                msol = run_minizinc_model(mdl_name, instance_name, timeout)
+                params.append([mdl_name, instance_name, timeout])
+    ts = time.time()
 
-                if msol:
-                    results['ids'].append(mdl_i)
-                    num_actors = instance_name.split("_")[1]
-                    num_scenes = instance_name.split("_")[2]
-                    results['num_actors'].append(num_actors)
-                    results['num_scenes'].append(num_scenes)
-                    results["obj"].append(msol.solution.objective)
-                    results["init_time"].append(msol.statistics['initTime'].total_seconds())
-                    results["solve_time"].append(msol.statistics['solveTime'].total_seconds())
+    # params = params[:10]
+    pool = mp.Pool(processes=mp.cpu_count()-1)
 
-                    results['instance_name'].append(instance_name)
-                    results['num_vars'].append(msol.statistics['variables'])
-                    results["sln_status"].append(msol.status.name)
+    outputs = pool.map(run_minizinc_model, params)
+    get_result(outputs)
 
-                    if "1" in mdl_name:
-                        scene_order = msol['scene_order']
-                    else:
-                        scene_order = msol['order']
-                    results['scene_order'].append(scene_order)
+    pool.close()
+    pool.join()
 
-                else:
-                    no_sln['ids'].append(mdl_i)
-                    no_sln['num_scenes'].append(num_scenes)
-                    no_sln['num_actors'].append(num_actors)
-                    no_sln['instance_name'].append(instance_name)
-                    no_sln["sln_status"].append(msol.status.name)
+    print('Time in parallel:', time.time() - ts)
+    print(results)
 
 
     with open("results.json", 'w+') as out:
@@ -116,6 +157,17 @@ if __name__ == "__main__":
 
     with open("no_results.json", 'w+') as out:
         json.dump(no_sln, out)
+
+if __name__ == "__main__":
+
+    results = {"ids":[], "num_scenes":[], "num_actors":[], "obj":[], "init_time":[], "solve_time":[], "num_vars":[], "sln_status":[], "instance_name":[], "scene_order":[], "mdl_name":[]}
+    no_sln = {"ids": [], "num_scenes":[], "num_actors":[], "sln_status": [], "instance_name":[], "mdl_name":[]}
+
+    # Params
+    timeout = 600
+    mdls = ['talent_model1.mzn', 'talent_model2.mzn']
+    run(mdls, timeout)
+
 
 
 
